8890d86589c30ff1678d0a4f4ed2a3701551d605
Ivan Dorofeev <axxel123@rambler.ru>
2022-09-29 10:32:30 +0300

    Create places app

---
+++ where_to_go/places/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    # path('point', views.point, name='point'),
+]
--- where_to_go/where_to_go/views.py
+++ where_to_go/where_to_go/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def main(request):
     context = {'title': 'Здесь скоро появится карта. Ура!', }
-    return render(request, 'main.html', context)
+    return render(request, 'index.html', context)
