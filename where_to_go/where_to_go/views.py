639f6384ba6f11f20132852cde7020429b388edd
Ivan Dorofeev <axxel123@rambler.ru>
2022-10-04 14:14:06 +0300

    Move data from template to view

--- where_to_go/where_to_go/views.py
+++ where_to_go/where_to_go/views.py
@@ -2,5 +2,36 @@ from django.shortcuts import render
 
 
 def main(request):
-    context = {'title': 'Здесь скоро появится карта. Ура!', }
+    geo_data = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.62, 55.793676]
+                },
+                "properties": {
+                    "title": "«Легенды Москвы",
+                    "placeId": "moscow_legends",
+                    "detailsUrl": "static/places/moscow_legends.json"
+                }
+            },
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [37.64, 55.753676]
+                },
+                "properties": {
+                    "title": "Крыши24.рф",
+                    "placeId": "roofs24",
+                    "detailsUrl": "static/places/roofs24.json"
+                }
+            }
+        ]
+    }
+    context = {
+        'geo_data': geo_data
+    }
     return render(request, 'index.html', context)
