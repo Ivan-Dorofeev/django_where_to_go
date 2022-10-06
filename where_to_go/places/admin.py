582225382014d1659386b1336f9d9f8cfbcf7f6f
Ivan Dorofeev <axxel123@rambler.ru>
2022-10-06 17:49:19 +0300

    add view for image in admin test

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.utils.html import format_html_join, format_html
+from django.utils.safestring import mark_safe
 
 from .models import Location, Image
 
@@ -8,16 +9,13 @@ class ImagesInline(admin.TabularInline):
     model = Image
     readonly_fields = ('image_report',)
 
-    def image_report(self, instance):
-        try:
-            result = format_html_join(
-                format_html('<br>'),'{}',
-                ((line,) for line in instance.images()), ) or format_html(
-                "<span class='errors'>I can't imaging this image.</span>")
-        except Exception as exc:
-            print('8' * 100)
-            print(exc)
-        return result
+    def image_report(self, obj):
+        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+            url=f'where_to_go/media{obj.images.url}',
+            width=200,
+            height=200,
+        )
+        )
 
 
 @admin.register(Location)
