20f4a98a7afd75c3fe1381e32709718f6038f17e
Ivan Dorofeev <axxel123@rambler.ru>
2022-12-06 09:54:41 +0300

    ready for deploy

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -11,11 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = secret_key
 
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -39,12 +39,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-# CSRF_COOKIE_SECURE = False
-# SECURE_SSL_REDIRECT = False
-# SESSION_COOKIE_SECURE = False
-# SECURE_HSTS_PRELOAD = True
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_SECONDS = 3600
+CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_SECONDS = 3600
 
 ROOT_URLCONF = 'where_to_go.urls'
 
