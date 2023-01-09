9d4b117f2a36a53e58e761c8765cc4bc55dbc804
Ivan Dorofeev <axxel123@rambler.ru>
2023-01-09 16:53:45 +0300

    test mew admin

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,25 +1,42 @@
 from django.contrib import admin
 from django.utils.html import format_html
 from .models import Location, Image
-from adminsortable2.admin import SortableStackedInline, SortableAdminBase
+from adminsortable2.admin import SortableInlineAdminMixin
 
 
-class ImagesInline(admin.TabularInline, SortableStackedInline):
+class PlaceImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
-    readonly_fields = ('image_report',)
+    readonly_fields = ['image_preview', ]
+    fields = ['image', 'image_preview', 'position']
 
-    def image_report(self, obj):
-        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
-            url=obj.image.url,
-            width=200,
-            height=200, ))
+    def image_preview(self, obj):
+        return obj.image_preview
 
 
 @admin.register(Location)
-class LocationAdmin(SortableAdminBase, admin.ModelAdmin):
-    inlines = [ImagesInline, ]
+class PlaceAdmin(admin.ModelAdmin):
+    search_fields = ['title', ]
+    inlines = [
+        PlaceImageInline,
+    ]
 
 
-@admin.register(Image)
-class ImagesAdmin(admin.ModelAdmin):
-    fields = ('location', 'image')
+# class ImagesInline(admin.TabularInline, SortableStackedInline):
+#     model = Image
+#     readonly_fields = ('image_report',)
+#
+#     def image_report(self, obj):
+#         return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+#             url=obj.image.url,
+#             width=200,
+#             height=200, ))
+#
+#
+# @admin.register(Location)
+# class LocationAdmin(SortableAdminBase, admin.ModelAdmin):
+#     inlines = [ImagesInline, ]
+#
+#
+# @admin.register(Image)
+# class ImagesAdmin(admin.ModelAdmin):
+#     fields = ('location', 'image')
